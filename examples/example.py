de12fb946234c4cc2b0bfc782b15cb2331a7c813
SharpBit <31069084+SharpBit@users.noreply.github.com>
2018-01-26 22:41:27 -0500

    fix example

--- examples/example.py
+++ examples/example.py
@@ -1,26 +1,26 @@
-# Import the module.
-import pynite
+import pynite  # this is the module
+import aiohttp
+import asyncio
 
-# Put your API Key here.
-token = ''
+token = ''  # this is your api key from fortnitetracker.com/site-api
+
 
 # Async loop.
 async def main():
-  
-    # Initialize the Client.
+
     client = pynite.Client(token, session=aiohttp.ClientSession())
 
-    # Fill in these variables!
-    platform = ''
-    # Can be pc, psn, or xbl
-    name = ''
+    platform = 'pc'  # Platform can be pc, psn, or xbl
+    name = 'sharpbit'  # epics game username
 
     # Get a Profile.
     profile = await client.get_player(platform, name)
+    solos = await client.get_solos(platform, name)
 
     # Print your number of Kills in Solo. (Docs coming soon, and an easier way to access exact info.)
-    print(profile.stats.p2.kills.value)
+    print(profile.account_id)  # prints account id
+    print(solos.kills.value)  # prints the number of kills you have
 
 # Run the async loop!
 loop = asyncio.get_event_loop()
-loop.run_until_complete(main()) 
+loop.run_until_complete(main())
