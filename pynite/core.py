d71b906253d355ee990d20c15105d680541da0a3
SharpBit <31069084+SharpBit@users.noreply.github.com>
2018-01-26 19:02:14 -0500

    this is the concept

--- pynite/core.py
+++ pynite/core.py
@@ -1,5 +1,31 @@
+'''
+MIT License
+
+Copyright (c) 2018 RBC
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
+
+
 import aiohttp
-from .utils import API, _to_snake_case
+from .utils import API
+from .errors import NotResponding, Unauthorized, NoProfileFound, NotPlayedError
 from box import Box
 
 
@@ -23,8 +49,64 @@ class Client:
             try:
                 raw_data = await resp.json()
             except Exception as e:
-                return e  # until we implement errors
+                if 500 > e.code > 400:
+                    raise Unauthorized()
+                elif e.code >= 500:
+                    raise NotResponding()
+                else:
+                    raise NoProfileFound()
 
-        data = Box(raw_data)
+        data = Box(raw_data, camel_killer_box=True)
+        self.platform = platform
+        self.name = name
 
         return data
+
+    async def get_solos(self, platform, name):
+        profile = self.get_player(platform, name)
+        try:
+            return profile.stats.p2
+        except AttributeError:
+            raise NotPlayedError('solos')
+
+    async def get_duos(self, platform, name):
+        profile = self.get_player(platform, name)
+        try:
+            return profile.stats.p10
+        except AttributeError:
+            raise NotPlayedError('duos')
+
+    async def get_squads(self, platform, name):
+        profile = self.get_player(platform, name)
+        try:
+            return profile.stats.p9
+        except AttributeError:
+            raise NotPlayedError('squads')
+
+
+class Player(Client):
+
+    def __init__(self):
+        super().__init__(Client)
+        self.profile = Client.get_player(self.platform, self.name)
+
+    def __repr__(self):
+        return '<Player object>'
+
+    def get_solos(self):
+        try:
+            return self.profile.stats.p2
+        except AttributeError:
+            raise NotPlayedError('solos')
+
+    def get_duos(self):
+        try:
+            return self.profile.stats.p10
+        except AttributeError:
+            raise NotPlayedError('duos')
+
+    def get_squads(self):
+        try:
+            return self.profile.stats.p9
+        except AttributeError:
+            raise NotPlayedError('squads')
--- pynite/errors.py
+++ pynite/errors.py
@@ -1,3 +1,28 @@
+'''
+MIT License
+
+Copyright (c) 2018 RBC
+
+Permission is hereby granted, free of charge, to any person obtaining a copy
+of this software and associated documentation files (the "Software"), to deal
+in the Software without restriction, including without limitation the rights
+to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+copies of the Software, and to permit persons to whom the Software is
+furnished to do so, subject to the following conditions:
+
+The above copyright notice and this permission notice shall be included in all
+copies or substantial portions of the Software.
+
+THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+SOFTWARE.
+'''
+
+
 class NotResponding(Exception):
     def __init__(self):
         self.code = 504
@@ -5,24 +30,27 @@ class NotResponding(Exception):
         super().__init__(self.error)
 
 
-class RequestError(Exception):
-    '''Base class for request errors'''
-
-    def __init__(self, resp, data):
-        self.response = resp
-        self.code = getattr(resp, 'status', None) or getattr(resp, 'status_code')
-        self.method = getattr(resp, 'method', None)
-        self.reason = resp.reason
-        if isinstance(data, dict):
-            self.error = data.get('error')
-            if 'message' in data:
-                self.error = data.get('message')
-        else:
-            self.error = data
-        self.fmt = '{0.reason} ({0.code}): {0.error}'.format(self)
-        super().__init__(self.fmt)
-
-
-class Unauthorized(RequestError):
+class Unauthorized(Exception):
     '''Raised when an invalid or blocked API key is passed'''
-    pass
+
+    def __init__(self):
+        self.code = 401
+        self.error = 'Invalid API key.'
+        super().__init__(self.error)
+
+
+class NoProfileFound(Exception):
+    '''Raised when an invalid platform/name combo has been passed'''
+
+    def __init__(self):
+        self.code = 404
+        self.error = 'No profile with this platform/name combination has been found.'
+        super().__init__(self.error)
+
+
+class NotPlayedError(Exception):
+    '''Raised when a player has not played a certain game mode'''
+
+    def __init__(self, mode):
+        self.error = f'This player has not played the {mode} gamemode yet.'
+        super().__init__(self.error)
