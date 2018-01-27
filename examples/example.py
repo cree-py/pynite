52fc482928678a840191bfaa03c77a6d5991cc01
the fixer of typos <30674647+umbresp@users.noreply.github.com>
2018-01-26 20:58:28 -0600

    Update example.py

--- examples/example.py
+++ examples/example.py
@@ -4,17 +4,23 @@ import pynite
 # Put your API Key here.
 token = ''
 
-# Initialize the Client.
-client = pynite.Client(token, session=aiohttp.ClientSession())
+# Async loop.
+async def main():
+  
+    # Initialize the Client.
+    client = pynite.Client(token, session=aiohttp.ClientSession())
 
-# Fill in these variables!
-platform = ''
-# Can be pc, psn, or xbl
-name = ''
+    # Fill in these variables!
+    platform = ''
+    # Can be pc, psn, or xbl
+    name = ''
 
-# Get a Profile.
-profile = await client.get_player(platform, name)
+    # Get a Profile.
+    profile = await client.get_player(platform, name)
 
-# Print your number of Kills in Solo. (Docs coming soon, and an easier way to access exact info.)
-print(profile.stats.p2.kills.value)
+    # Print your number of Kills in Solo. (Docs coming soon, and an easier way to access exact info.)
+    print(profile.stats.p2.kills.value)
 
+# Run the async loop!
+loop = asyncio.get_event_loop()
+loop.run_until_complete(main()) 
