4412533d9a4dbb35c75686a694693142fab5d0b4
the fixer of typos <30674647+umbresp@users.noreply.github.com>
2018-01-29 07:11:25 -0600

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     description='An async Python API wrapper for the Fortnite API',
     long_description="async python wrapper for the Fortnite API. Also, we don't yet have a good description so this empty page will have to do.",
     url='https://github.com/cree-py/pynite',
-    author='SharpBit(core) Umbresp(docs)',
+    author='SharpBit & Umbresp',
     author_email='creepy.org3301@gmail.com',
     license='MIT',
     keywords=['fortnite, pynite, api-wrapper, async'],
