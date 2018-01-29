4a5a7011d33eb9465addc8e24853bb4e6318a2a0
SharpBit <31069084+SharpBit@users.noreply.github.com>
2018-01-29 08:57:18 -0500

    Removewhitespaces

--- examples/discord.py
+++ examples/discord.py
@@ -5,12 +5,10 @@ import pynite
 class Fortnite:
     '''Example cog for a Discord bot made through the discord.py ext.commands framework.'''
 
-
     def __init__(self, bot):
         self.bot = bot
         self.client = pynite.Client('insert your token here', timeout=5)
 
-    
     @commands.command()
     async def profile(self, ctx, platform, name):
         '''Fetch a profile.'''
@@ -19,6 +17,7 @@ class Fortnite:
         solos = await player.get_solos()
 
         await ctx.send(f"# of kills in solos for {name}: {solos.kills.value}")
+
 
 def setup(bot):
     bot.add_cog(Fortnite(bot))
