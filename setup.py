68c094a0238f0981bdd9380c7d94e8b58df0cd7e
SharpBit <31069084+SharpBit@users.noreply.github.com>
2018-01-28 22:44:23 -0500

    Fix jeywords

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ setup(
     description='An async Python API wrapper for the Fortnite API',
     long_description="Async python wrapper for the Fortnite API. Also, we don't yet have a good description so this empty page will have to do.",
     url='https://github.com/cree-py/pynite',
-    author='SharpBit and Umbresp',
+    author='SharpBit(core) Umbresp(docs)',
     author_email='creepy.org3301@gmail.com',
     license='MIT',
-    keywords=['fortnite pynite api-wrapper async'],
+    keywords=['fortnite, pynite, api-wrapper, async'],
     packages=find_packages(),
     install_requires=['aiohttp', 'python-box']
 )
