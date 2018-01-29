846568f88c54a0cbc5ddc97b2488682af8c1e966
SharpBit <31069084+SharpBit@users.noreply.github.com>
2018-01-28 22:44:40 -0500

    a

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ setup(
     name='pynite',
     version='1.1.4',
     description='An async Python API wrapper for the Fortnite API',
-    long_description="Async python wrapper for the Fortnite API. Also, we don't yet have a good description so this empty page will have to do.",
+    long_description="async python wrapper for the Fortnite API. Also, we don't yet have a good description so this empty page will have to do.",
     url='https://github.com/cree-py/pynite',
     author='SharpBit(core) Umbresp(docs)',
     author_email='creepy.org3301@gmail.com',
