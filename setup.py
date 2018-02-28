30bfe394487143b27ebc900b3b5086b5bda0a682
SharpBit <31069084+SharpBit@users.noreply.github.com>
2018-02-28 15:46:15 -0500

    omg ugh

--- setup.py
+++ setup.py
@@ -1,13 +1,10 @@
 from setuptools import setup, find_packages
 
-with open("README.md", "r") as readme_file:
-    long_description = readme_file.read()
-
 setup(
     name='pynite',
     version='1.4.0',
     description='An async Python API wrapper for the Fortnite API',
-    long_description=long_description,
+    long_description='Powered by fortnitetracker.com. Async python 3.5+',
     url='https://github.com/cree-py/pynite',
     author='SharpBit & Umbresp',
     author_email='creepy.org3301@gmail.com',
